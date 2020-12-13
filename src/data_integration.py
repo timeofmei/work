f11d577dc32653391338bc8bb2bb41eada8be087
timeofmei <timeofmei@gmail.com>
2020-12-13 17:53:46 +0800

    district classification added

--- src/data_integration.py
+++ src/data_integration.py
@@ -1,7 +1,8 @@
 import json
 import pandas as pd
-river_list = ['洪阵河', '龙津河', '陈厝河', '白湖亭河', '洋里溪', '竹屿河', '瀛洲河', '竹榄河', '大庆河', '台屿河', '淌洋河', '马洲支河', '君竹河', '飞凤河', '泮洋河', '文藻河', '打铁港', '东郊河',
+river_list = ['洪阵河', '龙津河', '陈厝河', '白湖亭河', '洋里溪', '竹屿河', '瀛洲河', '竹榄河', '大庆河', '台屿河', '淌洋河', '马洲支河', '君竹河', '飞凤河', '泮洋河', '文藻河', '打铁港', '东郊河','安泰河',
               '新透河', '济南河', '磨洋河', '红星河', '茶园河', '琴亭河', '三捷河', '跃进河', '半洋亭河', '洋下河', '牛浦河', '浦东河', '浦上河', '阳岐河', '光明港二支河', '潘墩河', '金港河', '达道河', '梅峰河', '吴山河']
+fuzhou_districts = ['鼓楼', '台江', '仓山', '晋安', '马尾', '长乐']
 with open('data/content_wb.json') as file:
     data_wb = json.load(file)
 with open('data/content_fzrb.json') as file:
@@ -14,6 +15,7 @@ articles = []
 times = []
 sources = []
 rivers = []
+districts = []
 for keyword, ids in data_wb.items():
     for content in ids.values():
         keywords.append(content['keyword'])
@@ -57,9 +59,25 @@ for i in range(len(keywords)):
         rivers.append('未分类')
 assert len(keywords) == len(names) == len(
     articles) == len(times) == len(sources)
+for i in range(len(keywords)):
+    k = 0
+    for district in fuzhou_districts:
+        if district in articles[i]:
+            k += 1
+            try:
+                temp = districts[i]
+                temp += f',{district}'
+                districts[i] = temp
+            except IndexError:
+                districts.append(district)
+    if k == 0:
+        districts.append('未分类')
+assert len(keywords) == len(names) == len(
+    articles) == len(times) == len(sources)
 all_content = {
     '关键词': keywords,
     '河流': rivers,
+    '区域': districts,
     '作者': names,
     '正文': articles,
     '时间': times,
