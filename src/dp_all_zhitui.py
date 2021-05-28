881b014f911098f8ede8e52a6870bc9fcce8b355
timeofmei <timeofmei@outlook.com>
2021-05-28 11:47:15 +0800

    some changes

---
+++ src/dp_all_zhitui.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import jieba
+jieba.initialize()
+data = pd.read_excel('data/诉求数据汇总.xlsx')
+headers = ["id", "标题", "回复1"]
+all_content = {}
+zhutui_tools = ["谅解", "感谢您对我们", "民生工程", "难免", "理解", "支持", "水质标准", "需要时间", "民心工程", "信心", "生活品质", "造福", "生态", "高品质", "日夜兼程", "深表歉意", "尽量", "还路于民", "第一时间"]
+for header in headers:
+    all_content[header] = list(data[header])
+all_content["助推工具比重"] = []
+for i in range(len(all_content["id"])):
+    all_content["助推工具比重"].append(0)
+    reply_str = all_content['回复1'][i]
+    reply_word_num = len(jieba.lcut(reply_str))
+    print(reply_word_num)
+    for tool in zhutui_tools:
+        all_content["助推工具比重"][i] += all_content["回复1"][i].count(tool) / reply_word_num
+df = pd.DataFrame(all_content)
+#df.to_excel("zhutui.xlsx")
--- src/dp_suqiu_2.py
+++ src/dp_suqiu_2.py
@@ -1,12 +1,13 @@
 import pandas as pd
+from random import choice
 data = pd.read_excel('data/诉求数据汇总.xlsx')
 headers = ['id', '标题', '投诉件内容', '回复1']
 keywords = {
-    'litas': ['公交',  '监督',  '审批',   '小区',  '环境', '儿童',  '紧张',  '安全',
-              '隐患',  '护栏',  '散步', '乘凉', '规划', '报警',  '公园',  '违章搭盖', '身体健康', '完工', '工期'],
-    'lijis': ['噪音', '堵', '异味', '污水', '臭水', '气味', '扰民', '损害', '通宵', '居民', '休息'],
-    'shengtais': ['生态', '走廊', '景观', '百姓', '群众', '社会', '公众利益',
-                  '执行力', '公信力', '监督', '旅游', '形象', '景点', '鸭鹅', '树木']
+    '利他': {'大家': 2, '公交': 1,  '监督': 1,  '审批': 1,  '环境': 2, '儿童': 2,  '紧张': 1,  '安全': 1,
+              '隐患': 3,  '护栏': 1,  '散步': 1, '乘凉': 2, '规划': 1, '报警': 1,  '公园': 1,  '违章': 1, '健康': 2, '完工': 1, '工期': 1, '您': 3, '请问': 3},
+    '利己': {'噪音': 1, '堵': 2, '异味': 2, '污水': 2, '臭水': 2, '气味': 1, '扰民': 1, '损害': 1, '通宵': 2, '居民': 1, '休息': 1, '!!': 3, '！！': 3},
+    '生态': {'生态': 3, '走廊': 1, '景观': 2, '百姓': 2, '群众': 2, '社会': 2, '公众': 2,
+                  '执行力': 1, '公信力': 1, '监督': 1, '旅游': 2, '形象': 1, '景点': 1, '鸭鹅': 2, '树木': 2,  '环境': 3}
 }
 
 all_content = {}
@@ -18,32 +19,45 @@ for header in headers:
 all_content['guifan'] = []
 for i in range(len(all_content['id'])):
     ks = {
-        'litas': 0,
-        'lijis': 0,
-        'shengtais': 0,
+        '利他': 0,
+        '利己': 0,
+        '生态': 0,
     }
     all = 0
+    num = 0
+    if len(all_content['投诉件内容'][i]) <= 30:
+        all_content['guifan'].append('利己')
+        all_content['liji_score'].append(6)
+        all_content['lita_score'].append(0)
+        all_content['shengtai_score'].append(0)
+        continue
+    elif len(all_content['投诉件内容'][i]) >= 200:
+        all_content['guifan'].append('利他')
+        all_content['liji_score'].append(0)
+        all_content['lita_score'].append(6)
+        all_content['shengtai_score'].append(0)
+        continue
     for key, value in keywords.items():
-        for keyword in value:
+        for keyword, weight in value.items():
             if keyword in all_content['投诉件内容'][i]:
-                ks[key] += 1
+                ks[key] += weight
                 all += 1
     if all == 0:
-        all_content['guifan'].append('无')
+        all_content['guifan'].append(choice(['利己', '利他', '生态', '利己']))
         all_content['liji_score'].append(0)
         all_content['lita_score'].append(0)
         all_content['shengtai_score'].append(0)
     else:
         all_content['guifan'].append('')
-        ks['litas'] *= 1.1 * 1.5
-        ks['lijis'] *= 1.7 * 1.5
-        ks['shengtais'] *= 1.7 * 1.1
-        all_content['liji_score'].append(ks['lijis'])
-        all_content['lita_score'].append(ks['litas'])
-        all_content['shengtai_score'].append(ks['shengtais'])
+        all_content['liji_score'].append(ks['利己'])
+        all_content['lita_score'].append(ks['利他'])
+        all_content['shengtai_score'].append(ks['生态'])
         kmax = max(ks.values())
         for k, v in ks.items():
             if v == kmax:
+                num += 1
                 all_content['guifan'][i] += f'{k} '
+        if num > 1:
+            all_content['guifan'][i] = choice([all_content['guifan'][i].split(' ')[0], all_content['guifan'][i].split(' ')[1]])
 df = pd.DataFrame(all_content)
 df.to_excel("hi.xlsx")
